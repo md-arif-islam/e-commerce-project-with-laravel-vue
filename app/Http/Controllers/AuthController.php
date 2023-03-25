ce78251d2da5db14587fedfd8edbe1f683d2736c
md-arif-islam <arifislamarif344@gmail.com>
2023-03-25 17:36:44 +0600

    refactor(auth): update AuthController and UserResource
    The commit includes changes to the AuthController and UserResource files. The AuthController file now has the login, logout, and getUser methods, with updated functionalities for user authentication and token creation. The UserResource file is a new file that defines the user resource object.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller {
+    public function login( Request $request ) {
+
+        $credentials = $request->validate( [
+            'email' => ['required', 'email'],
+            'password' => 'required',
+            "remember" => "boolean",
+        ] );
+
+        $remember = $credentials['remember'] ?? false;
+
+        unset( $credentials['remember'] );
+
+        if ( Auth::attempt( $credentials, $remember ) ) {
+            return response( ['message' => "Email or password is incorrect!"], 422 );
+        }
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        if ( !$user->is_admin ) {
+            Auth::login();
+            return response( ['message' => "You don't have permission to authenticaate as admin!"], 403 );
+        }
+
+        $token = $user->createToken( "main" )->plainTextToken;
+
+        return response( [
+            "user" => new UserResource( $user ),
+            "token" => $token,
+        ] );
+    }
+
+    public function logout() {
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $user->currentAccessToken()->delete;
+
+        return response( ['', 204] );
+
+    }
+
+    public function getUser( Request $request ) {
+        return new UserResource( $request->user() );
+    }
+
+}
