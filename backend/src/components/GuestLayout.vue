f6292822ccda0483d6657ba212262019e6744f61
md-arif-islam <arifislamarif344@gmail.com>
2023-03-25 15:50:11 +0600

    refactor(layout): add submit event to GuestLayout form

--- backend/src/components/GuestLayout.vue
+++ backend/src/components/GuestLayout.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full space-y-8">
-      <div>
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          {{ title }}
-        </h2>
-      </div>
-      <slot></slot>
+    <div
+        class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    >
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                <h2
+                    class="mt-6 text-center text-3xl font-extrabold text-gray-900"
+                >
+                    {{ title }}
+                </h2>
+            </div>
+
+            <form
+                class="mt-8 space-y-6"
+                @submit.prevent="emit('submit')"
+                method="POST"
+            >
+                <slot></slot>
+            </form>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-  const {title} = defineProps({
-    title: String
-  })
+const { title } = defineProps({
+    title: String,
+});
+
+const emit = defineEmits(["submit"]);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
